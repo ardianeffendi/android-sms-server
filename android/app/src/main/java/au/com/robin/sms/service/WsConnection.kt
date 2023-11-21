0d7b767321a4365bdd8323dd63aa1b0a81605601
Ardian Effendi <ardian.effendi21@gmail.com>
2023-11-21 09:49:23 +0800

    remove globalId, listenerId and thread-safe blocks as there will be only one WsConnection instance

--- android/app/src/main/java/au/com/robin/sms/service/WsConnection.kt
+++ android/app/src/main/java/au/com/robin/sms/service/WsConnection.kt
@@ -7,10 +7,9 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicLong
 
 /**
- * Model after ntfy-android/WsConnection.kt.
+ * Modelled after ntfy-android/WsConnection.kt.
  * (https://github.com/binwiederhier/ntfy-android/blob/main/app/src/main/java/io/heckel/ntfy/service/WsConnection.kt)
  */
 class WsConnection : Connection {
@@ -22,19 +21,15 @@ class WsConnection : Connection {
     private var state: State? = null
     private var closed = false
 
-    private val globalId = GLOBAL_ID.incrementAndGet()
-    private val listenerId = AtomicLong(0)
 
     init {
-        Log.d(TAG, "New connection with global ID $globalId")
+        Log.d(TAG, "New connection initialised...")
     }
 
-    @Synchronized
     override fun start() {
         if (closed || state == State.Connecting || state == State.Connected) {
             Log.d(
-                TAG,
-                "(gid=$globalId): Not (re-)starting, because connection is marked closed/connecting/connected"
+                TAG, "Not (re-)starting, because connection is marked closed/connecting/connected"
             )
             return
         }
@@ -42,23 +37,20 @@ class WsConnection : Connection {
             webSocket!!.close(WS_CLOSE_NORMAL, "")
         }
         state = State.Connecting
-        val nextListenerId = listenerId.incrementAndGet()
         val request = requestBuilder(SERVER_URL).build()
-        Log.d(TAG, "Opening connection with listener ID $nextListenerId")
-        webSocket = client.newWebSocket(request, Listener(nextListenerId))
+        Log.d(TAG, "Opening connection...")
+        webSocket = client.newWebSocket(request, Listener())
     }
 
-    @Synchronized
     override fun close() {
         closed = true
         if (webSocket == null) {
             Log.d(
-                TAG,
-                "(gid=$globalId): Not closing existing connection because there is no active websocket"
+                TAG, "Not closing existing connection because there is no active websocket"
             )
             return
         }
-        Log.d(TAG, "(gid=$globalId): Closing connection")
+        Log.d(TAG, "Closing connection")
         state = State.Disconnected
         webSocket!!.close(WS_CLOSE_NORMAL, "")
         webSocket = null
@@ -66,71 +58,47 @@ class WsConnection : Connection {
 
     // TODO(Implement function `scheduleReconnect()`)
 
-    private inner class Listener(private val id: Long) : WebSocketListener() {
+    private inner class Listener : WebSocketListener() {
         override fun onOpen(webSocket: WebSocket, response: Response) {
-            synchronize("onOpen") {
-                Log.d(TAG, "(gid=$globalId): Opened connection")
-                state = State.Connected
-                if (errorCount > 0) {
-                    errorCount = 0
-                }
+            Log.d(TAG, "Opened connection")
+            state = State.Connected
+            if (errorCount > 0) {
+                errorCount = 0
             }
         }
 
         override fun onMessage(webSocket: WebSocket, text: String) {
-            synchronize("onMessage") {
-                Log.d(TAG, "(gid=$globalId, lid=$id): Received message: $text")
-                // TODO(Implement a callback to pass the message to UI)
-            }
+            Log.d(TAG, "Received message: $text")
+            // TODO(Implement a callback to pass the message to UI)
         }
 
         override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-            synchronize("onClosed") {
-                Log.w(TAG, "(gid=$globalId, lid=$id): Closed connection")
-                state = State.Disconnected
-            }
+            Log.w(TAG, "Closed connection")
+            state = State.Disconnected
         }
 
         override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-            synchronize("onFailure") {
-                if (response == null) {
-                    Log.e(
-                        TAG,
-                        "(gid=$globalId, lid=$id): Connection failed (response is null): ${t.message}",
-                        t
-                    )
-                } else {
-                    Log.e(
-                        TAG,
-                        "(gid=$globalId, lid=$id): Connection failed (response code ${response.code}, message: ${response.message}): ${t.message}",
-                        t
-                    )
-                }
-                if (closed) {
-                    Log.d(
-                        TAG,
-                        "(gid=$globalId, lid=$id): Connection marked as closed. Not retrying."
-                    )
-                    return@synchronize
-                }
-                state = State.Disconnected
-                errorCount++
-                val retrySeconds = RETRY_SECONDS.getOrNull(errorCount) ?: RETRY_SECONDS.last()
-                // Call scheduleReconnect() - not yet implemented
+            if (response == null) {
+                Log.e(
+                    TAG, "Connection failed (response is null): ${t.message}", t
+                )
+            } else {
+                Log.e(
+                    TAG,
+                    "Connection failed (response code ${response.code}, message: ${response.message}): ${t.message}",
+                    t
+                )
             }
-        }
-
-        private fun synchronize(tag: String, fn: () -> Unit) {
-            synchronized(this) {
-                if (listenerId.get() == id) {
-                    fn()
-                } else {
-                    Log.w(
-                        TAG,
-                        "(gid=$globalId): Skipping synchronized block '$tag' because listener ID does not match ${listenerId.get()}"
-                    )
-                }
+            if (closed) {
+                Log.d(
+                    TAG, "Connection marked as closed. Not retrying."
+                )
+                return
             }
+            state = State.Disconnected
+            errorCount++
+            val retrySeconds = RETRY_SECONDS.getOrNull(errorCount) ?: RETRY_SECONDS.last()
+            // Call scheduleReconnect() - not yet implemented
         }
     }
 
@@ -143,7 +111,6 @@ class WsConnection : Connection {
         private const val RECONNECT_TAG = "WsReconnect"
         private const val WS_CLOSE_NORMAL = 1000
         private val RETRY_SECONDS = listOf(5, 10, 15, 20, 30, 45, 60, 120)
-        private val GLOBAL_ID = AtomicLong(0)
 
         fun requestBuilder(url: String): Request.Builder {
             return Request.Builder().url(url)
