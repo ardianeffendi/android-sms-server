6654c2c7ae9b8912ad127273eda8fab6e3b8bb2f
Ardian Effendi <ardian.effendi21@gmail.com>
2023-11-25 14:52:33 +0800

    add buttons to start and stop the service

--- android/app/src/main/java/au/com/robin/sms/MainActivity.kt
+++ android/app/src/main/java/au/com/robin/sms/MainActivity.kt
@@ -1,9 +1,11 @@
 package au.com.robin.sms
 
 import android.Manifest
+import android.app.AlarmManager
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import au.com.robin.sms.develop.R
 import au.com.robin.sms.service.ServiceState
 import au.com.robin.sms.service.SubscriberService
+import au.com.robin.sms.service.WsConnection
 import au.com.robin.sms.service.getServiceState
 
 // Constants
@@ -20,7 +23,7 @@ private const val SIM_SLOT_ONE = 0
 private const val SIM_SLOT_TWO = 1
 
 class MainActivity : AppCompatActivity() {
-    @RequiresPermission(allOf = [Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS])
+    @RequiresPermission(allOf = [Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS, Manifest.permission.FOREGROUND_SERVICE_DATA_SYNC])
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,33 +34,34 @@ class MainActivity : AppCompatActivity() {
         val etPhoneNo = findViewById<EditText>(R.id.et_phoneNo)
         val etMessage = findViewById<EditText>(R.id.et_message)
 
-        btSend.setOnClickListener {
-            val phoneNo = etPhoneNo.text?.toString()
-            val message = etMessage.text?.toString()
+        if (arePermissionsGranted(this)) {
+            findViewById<Button>(R.id.bt_start_service).let {
+                Log.d("MainActivity", "Start the foreground service on demand")
+                it.setOnClickListener {
+                    actionService(SubscriberService.Actions.START)
+                }
+            }
 
-            // Check for permission
-            if (isPermissionGranted(this)) {
+            findViewById<Button>(R.id.bt_stop_service).let {
+                Log.d("MainActivity", "Stop the foreground service on demand")
+                it.setOnClickListener {
+                    actionService(SubscriberService.Actions.STOP)
+                }
+            }
+
+            btSend.setOnClickListener {
+                val phoneNo = etPhoneNo.text?.toString()
+                val message = etMessage.text?.toString()
+
                 val intent = Intent()
                 // Default to SIM slot 1 for now
                 // This is supposed to be passed in with a value that user chooses.
                 intent.putExtra(SIM_SLOT, SIM_SLOT_ONE)
                 val smsManager = getSmsManager(this, intent)
                 smsManager?.sendTextMessage(phoneNo, null, message, null, null)
-            } else {
-                requestPermission(this)
             }
-        }
-
-        findViewById<Button>(R.id.bt_start_service).let {
-            it.setOnClickListener {
-                actionService(SubscriberService.Actions.START)
-            }
-        }
-
-        findViewById<Button>(R.id.bt_stop_service).let {
-            it.setOnClickListener {
-                actionService(SubscriberService.Actions.STOP)
-            }
+        } else {
+            requestPermissions(this)
         }
     }
 
@@ -72,6 +76,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Performs an action on the SubscriberService based on the provided action type.
+     *
+     * This function creates an intent for the SubscriberService with the specified action, allowing
+     * the service to start or stop based on the provided action. It checks the current state of the
+     * service to avoid redundant calls when stopping the service that is already in a stopped state.
+     * On Android Oreo (API level 26) and higher, it uses `startForegroundService` for compatibility.
+     *
+     * @param action The action to perform on the SubscriberService (START or STOP).
+     */
     private fun actionService(action: SubscriberService.Actions) {
         if (getServiceState(this) == ServiceState.STOPPED && action == SubscriberService.Actions.STOP) return
         Intent(this, SubscriberService::class.java).also {
